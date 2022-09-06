86d98cf54b803feff221cadfc571059f4a6683af
“Narendra” <narendrajadhav0707@gmail.com>
2022-09-06 13:45:53 +0530

    features/user_controller

--- db/migrate/20220905141606_create_friends.rb
+++ db/migrate/20220905141606_create_friends.rb
@@ -1,8 +1,9 @@
 class CreateFriends < ActiveRecord::Migration[6.1]
   def change
     create_table :friends do |t|
-      t.references :sent_to, null: false, foreign_key: true
-      t.references :sent_by, null: false, foreign_key: true
+      t.references :sent_by, null: false, foreign_key: { to_table: :users }
+      t.references :sent_to, null: false, foreign_key: { to_table: :users }
+      t.boolean :status, default: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_05_135532) do
+ActiveRecord::Schema.define(version: 2022_09_05_142401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,16 @@ ActiveRecord::Schema.define(version: 2022_09_05_135532) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "friends", force: :cascade do |t|
+    t.bigint "sent_by_id", null: false
+    t.bigint "sent_to_id", null: false
+    t.boolean "status", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["sent_by_id"], name: "index_friends_on_sent_by_id"
+    t.index ["sent_to_id"], name: "index_friends_on_sent_to_id"
+  end
+
   create_table "likes", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -34,6 +44,15 @@ ActiveRecord::Schema.define(version: 2022_09_05_135532) do
     t.index ["comment_id"], name: "index_likes_on_comment_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
     t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.integer "notice_id"
+    t.string "notice_type"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -61,8 +80,11 @@ ActiveRecord::Schema.define(version: 2022_09_05_135532) do
 
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
+  add_foreign_key "friends", "users", column: "sent_by_id"
+  add_foreign_key "friends", "users", column: "sent_to_id"
   add_foreign_key "likes", "comments"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "notifications", "users"
   add_foreign_key "posts", "users"
 end
